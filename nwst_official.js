e1256ff3971969812dff7c081c374e0495839cc7
336699c <63760951+336699c@users.noreply.github.com>
2022-08-14 09:37:11 +0800

    Update nwst_official.js

--- nwst_official.js
+++ nwst_official.js
@@ -1,9 +1,9 @@
-window.setTimeout(function(){document.getElementsByClassName("vpadding")[0].click();},200);
+window.setTimeout(function(){document.getElementsByClassName("vpadding")[selection[0]].click();},200);
 window.setTimeout(function(){document.getElementById("routelist2").children[2].childNodes[1].childNodes[0].click()},500);
 window.setTimeout(function(){hidespecialNote();select(0)},900);
 
 function select(x){
-document.getElementById("dtsysstoplist1").childNodes[1].childNodes[1].childNodes[x*2].click();
+document.getElementById("dtsysstoplist1").childNodes[1].childNodes[1].childNodes[selection[1]*2].click();
 
 window.setTimeout(function(){
 console.log(document.getElementById("vbus_stop_focus").innerText);
@@ -14,8 +14,3 @@ console.log(e.childNodes[1].childNodes[0].innerText)
 },500);
 
 }
-
-
-
-
-
