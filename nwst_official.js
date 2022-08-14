99d0368219c574cc5156bbf55ab505ab6f1ee442
336699c <63760951+336699c@users.noreply.github.com>
2022-08-14 10:24:30 +0800

    Update nwst_official.js

--- nwst_official.js
+++ nwst_official.js
@@ -8,7 +8,7 @@ function select(){
 document.getElementById("dtsysstoplist1").childNodes[1].childNodes[1].childNodes[selection[1]*2].click();
 
 window.setTimeout(function(){
-window.AppInventor.setWebViewString(document.getElementById("vbus_stop_focus").innerText);
+window.AppInventor.setWebViewString(document.getElementById("vbus_stop_focus").innerText+"\n");
 
 document.getElementById("nextbus_listitem").childNodes.forEach(e=>{
 window.AppInventor.setWebViewString(e.childNodes[1].childNodes[0].innerText.replace(/\n/g,"  "))
