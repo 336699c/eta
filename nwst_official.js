81f63b5f86772eca44e434cfbc749651bc5df231
336699c <63760951+336699c@users.noreply.github.com>
2022-08-14 09:51:03 +0800

    Update nwst_official.js

--- nwst_official.js
+++ nwst_official.js
@@ -1,16 +1,17 @@
-window.setTimeout(function(){document.getElementsByClassName("vpadding")[selection[0]].click();},200);
-window.setTimeout(function(){document.getElementById("routelist2").children[2].childNodes[1].childNodes[0].click()},500);
-window.setTimeout(function(){hidespecialNote();select(0)},900);
 
-function select(x){
+window.setTimeout(function(){document.getElementsByClassName("vpadding")[selection[0]].click();},200);
+window.setTimeout(function(){document.getElementById("routelist2").children[2].childNodes[1].childNodes[0].click()},1000);
+window.setTimeout(function(){hidespecialNote();select()},2000);
+
+function select(){
 document.getElementById("dtsysstoplist1").childNodes[1].childNodes[1].childNodes[selection[1]*2].click();
 
 window.setTimeout(function(){
 console.log(document.getElementById("vbus_stop_focus").innerText);
 
 document.getElementById("nextbus_listitem").childNodes.forEach(e=>{
-window.AppInventor.setWebViewString(e.childNodes[1].childNodes[0].innerText)
+console.log(e.childNodes[1].childNodes[0].innerText)
 })
-},500);
+},1000);
 
 }
